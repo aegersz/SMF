65cbf058cc8969cc4fd99b5a5fd44f566daad9d9
Andrew <aegersz@hotmail.com>
2018-10-03 00:54:48 +0800

    typo

--- dev/db_links_convert_smf.sh
+++ dev/db_links_convert_smf.sh
@@ -3,7 +3,7 @@
 live="forum.site-name.org/index.php/topic"
 dev="192.168.0.180/index.php/topic"
 
-echo coverting all occurrences of $live to $dev ...
+echo converting all occurrences of $live to $dev ...
 sed "s|$live|$dev|g" /rsync-live-dbbackup > /rsync-live-dbbackup-newlinks
 echo "sed;"$?
 
