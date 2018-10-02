6f320cb778c5f99505913ae21a82b1fb76d7331d
Andrew <aegersz@hotmail.com>
2018-10-03 00:36:52 +0800

    security

--- dev/db_links_convert_smf.sh
+++ dev/db_links_convert_smf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-live="forum.drugs-and-users.org/index.php/topic"
+live="forum.site-name.org/index.php/topic"
 dev="192.168.0.180/index.php/topic"
 
 sed "s|$live|$dev|g" /rsync-live-dbbackup > /rsync-live-dbbackup-newlink
