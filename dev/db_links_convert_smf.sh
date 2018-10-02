7da396da889b6d4e8631d9d3eaea673e4e9ea809
Andrew <aegersz@hotmail.com>
2018-10-03 00:41:10 +0800

    typo

--- dev/db_links_convert_smf.sh
+++ dev/db_links_convert_smf.sh
@@ -3,6 +3,6 @@
 live="forum.site-name.org/index.php/topic"
 dev="192.168.0.180/index.php/topic"
 
-sed "s|$live|$dev|g" /rsync-live-dbbackup > /rsync-live-dbbackup-newlink
+sed "s|$live|$dev|g" /rsync-live-dbbackup > /rsync-live-dbbackup-newlinks
 echo $?
 
